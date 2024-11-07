82911ff2f38b14955310d89838507121e60a5472
iRpro16 <rumeoisidoro16@gmail.com>
2024-11-07 12:04:27 -0500

    fetch API and data

--- src/components/content.js
+++ src/components/content.js
@@ -3,13 +3,26 @@ import clouds from "/src/assets/images/clouds.jpg";
 
 const createContent = (function() {
     const loadBackImage = () => {
+        // set url and size to cover
         document.body.style.backgroundImage = `url(${clouds})`;
         document.body.style.backgroundSize = 'cover';
     }
 
     const loadContent = () => {
+        // get content
+        const content = document.querySelector(".content");
+
         // load background image
         loadBackImage();
+
+        // create central div
+        const centralDiv = document.createElement('div');
+        centralDiv.classList.add('central-div');
+        content.append(centralDiv);
+    }
+
+    const loadWeatherInfo = (location) => {
+
     }
 
     return {loadContent};
---
+++ src/controllers/contentcontroller.js
@@ -0,0 +1,31 @@
+export {contentController};
+
+const contentController = (function() {
+    // function to fetch API
+    const fetchWeatherAPI = async(url) => {
+        // try / catch
+        try {
+            // fetch url
+            const response = await fetch(url, {mode:'cors'});
+            if (response.ok) {
+                return response.json();
+            }
+        // catch error
+        } catch (error) {
+            console.error('Error fetching data', error);
+            return null;
+        }
+    }
+
+    // function to fetch data 
+    const fetchData = async(location) => {
+        // get location in url
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=32DPJC49QYH7EAHNB45B5CKBY&contentType=json`;
+
+        // await fetched data
+        const data = await fetchWeatherAPI(url);
+        return data
+    }
+
+    return {fetchData}
+})();
